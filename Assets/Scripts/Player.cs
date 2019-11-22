a502cef93e95bd3482a2df48f6871465a5857cf4
Christopher Tero <christopher.tero@gmail.com>
2019-11-22 11:46:34 -0600

    restricted player movement to within camera view

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitiateMoveBounderies();
+    }
+
+    private void InitiateMoveBounderies()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -22,8 +38,10 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime;
-        var newXPosition = transform.position.x + moveSpeed * deltaX;
-        var newYPosition = transform.position.y + moveSpeed * deltaY;
+
+        var newXPosition = Mathf.Clamp(transform.position.x + moveSpeed * deltaX, xMin, xMax);
+        var newYPosition = Mathf.Clamp(transform.position.y + moveSpeed * deltaY, yMin, yMax);
+
         transform.position = new Vector2(newXPosition, newYPosition);
     }
 }
