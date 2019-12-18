95d87b34b555e41e2ed69f20a67f0b2da96819bb
Christopher Tero <christopher.tero@gmail.com>
2019-12-17 20:26:54 -0600

    working on swapping scenes, still buggy

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
